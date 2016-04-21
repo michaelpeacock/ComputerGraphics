c84624286ec944b25d9172061c7e543ffad98dfe
michaelpeacock <michael.peacock@gmail.com>
2016-04-21 14:32:35 -0400

    cleanup

--- Project1/src/voltron/objects/Castle.java
+++ Project1/src/voltron/objects/Castle.java
@@ -13,6 +13,7 @@ import voltron.objects.LionFactory.LION_COLOR;
 public class Castle {
 	private Map<String, Integer> objectList = new HashMap<String, Integer>();
 	private GLCanvas glcanvas;
+	private Logo logo;
 	private LionFactory lionFactory;
 
 	public void initializeCastle(GLCanvas glcanvas, GLAutoDrawable drawable) {
@@ -20,14 +21,16 @@ public class Castle {
 
 		GL gl = drawable.getGL();
 		gl.glPushMatrix();
+
+		logo = new Logo();
+		logo.createLogo(drawable);
+
 		createLeft(drawable);
 		createRight(drawable);
-		createLogo(drawable);
 		createCenter(drawable);
 
 		lionFactory = new LionFactory(glcanvas);
 		lionFactory.createLion("Red", LION_COLOR.RED);
-		lionFactory.createLion("Black", LION_COLOR.BLACK);
 		lionFactory.createLion("Green", LION_COLOR.GREEN);
 		lionFactory.createLion("Blue", LION_COLOR.BLUE);
 		lionFactory.createLion("Yellow", LION_COLOR.YELLOW);
@@ -79,7 +82,7 @@ public class Castle {
 
 		gl.glPushMatrix();
 		gl.glTranslatef(25, 375, 605);
-		gl.glCallList(objectList.get("Logo"));
+		logo.display(drawable);
 		gl.glPopMatrix();
 
 		gl.glPushMatrix();
@@ -112,152 +115,6 @@ public class Castle {
 
 		gl.glPopMatrix();
 		gl.glEndList();
-	}
-
-	public void createLogo(GLAutoDrawable drawable) {
-		GL gl = drawable.getGL();
-
-		gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL);
-		// gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE);
-		objectList.put("Logo", gl.glGenLists(1));
-		gl.glNewList(objectList.get("Logo"), GL.GL_COMPILE);
-
-		// back of logo
-		gl.glPushMatrix();
-		gl.glColor3d(1, 0, 0);
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glVertex3d(0.0, 0.0, 0.0);
-		gl.glVertex3d(0.0, -125.0, 0.0);
-		gl.glVertex3d(25.0, -150.0, 0.0);
-		gl.glVertex3d(125.0, -150.0, 0.0);
-		gl.glVertex3d(150.0, -125.0, 0.0);
-		gl.glVertex3d(150.0, 0.0, 0.0);
-		gl.glVertex3d(0.0, 0.0, 0.0);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// inside of logo
-		gl.glPushMatrix();
-		gl.glColor3d(0, 0, 0);
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glVertex3d(5.0, -5.0, 0.05);
-		gl.glVertex3d(5.0, -120.0, 0.05);
-		gl.glVertex3d(30.0, -145.0, 0.05);
-		gl.glVertex3d(120.0, -145.0, 0.05);
-		gl.glVertex3d(145.0, -120.0, 0.05);
-		gl.glVertex3d(145.0, -5.0, 0.05);
-		gl.glVertex3d(5.0, -5.0, 0.05);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// grey inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0.753, 0.753, 0.753);
-		gl.glVertex3d(10.0, -10.0, 0.08);
-		gl.glVertex3d(10.0, -115.0, 0.08);
-		gl.glVertex3d(35.0, -140.0, 0.08);
-		gl.glVertex3d(115.0, -140.0, 0.08);
-		gl.glVertex3d(140.0, -115.0, 0.08);
-		gl.glVertex3d(140.0, -10.0, 0.08);
-		gl.glVertex3d(10.0, -10.0, 0.08);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// blue inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0, 0, 1);
-		gl.glVertex3d(15.0, -15.0, 0.5);
-		gl.glVertex3d(15.0, -70.0, 0.5);
-		gl.glVertex3d(75.0, -70.0, 0.5);
-		gl.glVertex3d(75.0, -15.0, 0.5);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// yellow inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(1, 1, 0);
-		gl.glVertex3d(75.0, -15.0, 0.5);
-		gl.glVertex3d(75.0, -70.0, 0.5);
-		gl.glVertex3d(135.0, -70.0, 0.5);
-		gl.glVertex3d(135.0, -15.0, 0.5);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// red inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(1, 0, 0);
-		gl.glVertex3d(15.0, -70.0, 0.5);
-		gl.glVertex3d(15.0, -115.0, 0.5);
-		gl.glVertex3d(35.0, -135.0, 0.5);
-		gl.glVertex3d(75.0, -135.0, 0.5);
-		gl.glVertex3d(75.0, -70.0, 0.5);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// green inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0, 0.6, 0);
-		gl.glVertex3d(75.0, -70.0, 0.5);
-		gl.glVertex3d(75.0, -135.0, 0.5);
-		gl.glVertex3d(115.0, -135.0, 0.5);
-		gl.glVertex3d(135.0, -115.0, 0.5);
-		gl.glVertex3d(135.0, -70.0, 0.5);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// black strip inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0, 0, 0);
-		gl.glVertex3d(70.0, -15.0, 0.8);
-		gl.glVertex3d(70.0, -135.0, 0.8);
-		gl.glVertex3d(80.0, -135.0, 0.8);
-		gl.glVertex3d(80.0, -15.0, 0.8);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// cross inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0.6, 0.298, 0);
-		gl.glVertex3d(73.0, -25.0, 0.9);
-		gl.glVertex3d(73.0, -130.0, 0.9);
-		gl.glVertex3d(77.0, -130.0, 0.9);
-		gl.glVertex3d(77.0, -25.0, 0.9);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(0.6, 0.298, 0);
-		gl.glVertex3d(20.0, -75.0, 0.9);
-		gl.glVertex3d(20.0, -80.0, 0.9);
-		gl.glVertex3d(130.0, -80.0, 0.9);
-		gl.glVertex3d(130.0, -75.0, 0.9);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		// crown inside of logo
-		gl.glPushMatrix();
-		gl.glBegin(GL.GL_POLYGON);
-		gl.glColor3d(1.0, 0.7686, 0.0);
-		gl.glVertex3d(65.0, -40.0, 1.0);
-		gl.glVertex3d(50.0, -20.0, 1.0); // top left
-		gl.glVertex3d(65.0, -35.0, 1.0);
-		gl.glVertex3d(75.0, -20.0, 1.0); // peak
-		gl.glVertex3d(85.0, -35.0, 1.0);
-		gl.glVertex3d(100.0, -20.0, 1.0); // top right
-		gl.glVertex3d(85.0, -40.0, 1.0);
-		gl.glEnd();
-		gl.glPopMatrix();
-
-		gl.glEndList();
-
 	}
 
 	public void createLeft(GLAutoDrawable drawable) {
@@ -447,16 +304,9 @@ public class Castle {
 		gl.glCallList(objectList.get("Center"));
 		gl.glPopMatrix();
 
-		// gl.glTranslated(0.0, -10.0, 600.0);
-		// gl.glCallList(objectList.get("Path"));
-		// gl.glPopMatrix();
-
 		gl.glPushMatrix();
 		gl.glScaled(0.5, 0.5, 0.5);
 		gl.glTranslated(0.0, 80.0, 4500.0);
-		gl.glPushMatrix();
-		lionFactory.getLion("Black").display(drawable);
-		gl.glPopMatrix();
 
 		gl.glPushMatrix();
 		gl.glTranslated(-500.0, 0.0, 0.0);
---
+++ Project1/src/voltron/objects/Logo.java
@@ -0,0 +1,159 @@
+package voltron.objects;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+
+public class Logo {
+
+	public void createLogo(GLAutoDrawable drawable) {
+		GL gl = drawable.getGL();
+
+		gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL);
+		gl.glGenLists(1);
+		gl.glNewList(1, GL.GL_COMPILE);
+
+		// back of logo
+		gl.glPushMatrix();
+		gl.glColor3d(1, 0, 0);
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glVertex3d(0.0, 0.0, 0.0);
+		gl.glVertex3d(0.0, -125.0, 0.0);
+		gl.glVertex3d(25.0, -150.0, 0.0);
+		gl.glVertex3d(125.0, -150.0, 0.0);
+		gl.glVertex3d(150.0, -125.0, 0.0);
+		gl.glVertex3d(150.0, 0.0, 0.0);
+		gl.glVertex3d(0.0, 0.0, 0.0);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// inside of logo
+		gl.glPushMatrix();
+		gl.glColor3d(0, 0, 0);
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glVertex3d(5.0, -5.0, 0.05);
+		gl.glVertex3d(5.0, -120.0, 0.05);
+		gl.glVertex3d(30.0, -145.0, 0.05);
+		gl.glVertex3d(120.0, -145.0, 0.05);
+		gl.glVertex3d(145.0, -120.0, 0.05);
+		gl.glVertex3d(145.0, -5.0, 0.05);
+		gl.glVertex3d(5.0, -5.0, 0.05);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// grey inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0.753, 0.753, 0.753);
+		gl.glVertex3d(10.0, -10.0, 0.08);
+		gl.glVertex3d(10.0, -115.0, 0.08);
+		gl.glVertex3d(35.0, -140.0, 0.08);
+		gl.glVertex3d(115.0, -140.0, 0.08);
+		gl.glVertex3d(140.0, -115.0, 0.08);
+		gl.glVertex3d(140.0, -10.0, 0.08);
+		gl.glVertex3d(10.0, -10.0, 0.08);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// blue inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0, 0, 1);
+		gl.glVertex3d(15.0, -15.0, 0.5);
+		gl.glVertex3d(15.0, -70.0, 0.5);
+		gl.glVertex3d(75.0, -70.0, 0.5);
+		gl.glVertex3d(75.0, -15.0, 0.5);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// yellow inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(1, 1, 0);
+		gl.glVertex3d(75.0, -15.0, 0.5);
+		gl.glVertex3d(75.0, -70.0, 0.5);
+		gl.glVertex3d(135.0, -70.0, 0.5);
+		gl.glVertex3d(135.0, -15.0, 0.5);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// red inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(1, 0, 0);
+		gl.glVertex3d(15.0, -70.0, 0.5);
+		gl.glVertex3d(15.0, -115.0, 0.5);
+		gl.glVertex3d(35.0, -135.0, 0.5);
+		gl.glVertex3d(75.0, -135.0, 0.5);
+		gl.glVertex3d(75.0, -70.0, 0.5);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// green inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0, 0.6, 0);
+		gl.glVertex3d(75.0, -70.0, 0.5);
+		gl.glVertex3d(75.0, -135.0, 0.5);
+		gl.glVertex3d(115.0, -135.0, 0.5);
+		gl.glVertex3d(135.0, -115.0, 0.5);
+		gl.glVertex3d(135.0, -70.0, 0.5);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// black strip inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0, 0, 0);
+		gl.glVertex3d(70.0, -15.0, 0.8);
+		gl.glVertex3d(70.0, -135.0, 0.8);
+		gl.glVertex3d(80.0, -135.0, 0.8);
+		gl.glVertex3d(80.0, -15.0, 0.8);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// cross inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0.6, 0.298, 0);
+		gl.glVertex3d(73.0, -25.0, 0.9);
+		gl.glVertex3d(73.0, -130.0, 0.9);
+		gl.glVertex3d(77.0, -130.0, 0.9);
+		gl.glVertex3d(77.0, -25.0, 0.9);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(0.6, 0.298, 0);
+		gl.glVertex3d(20.0, -75.0, 0.9);
+		gl.glVertex3d(20.0, -80.0, 0.9);
+		gl.glVertex3d(130.0, -80.0, 0.9);
+		gl.glVertex3d(130.0, -75.0, 0.9);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		// crown inside of logo
+		gl.glPushMatrix();
+		gl.glBegin(GL.GL_POLYGON);
+		gl.glColor3d(1.0, 0.7686, 0.0);
+		gl.glVertex3d(65.0, -40.0, 1.0);
+		gl.glVertex3d(50.0, -20.0, 1.0); // top left
+		gl.glVertex3d(65.0, -35.0, 1.0);
+		gl.glVertex3d(75.0, -20.0, 1.0); // peak
+		gl.glVertex3d(85.0, -35.0, 1.0);
+		gl.glVertex3d(100.0, -20.0, 1.0); // top right
+		gl.glVertex3d(85.0, -40.0, 1.0);
+		gl.glEnd();
+		gl.glPopMatrix();
+
+		gl.glEndList();
+	}
+
+	public void display(GLAutoDrawable drawable) {
+		GL gl = drawable.getGL();
+		gl.glPushMatrix();
+		gl.glCallList(1);
+		gl.glPopMatrix();
+	}
+
+}
